659a54dc90ab29aa398d8ecd74021977acae8ff4
Adam Sládeček <adamsladecek0@gmail.com>
2025-03-08 20:43:28 +0100

    change to postgre

--- scrape_server/scrape_server/test_settings.py
+++ scrape_server/scrape_server/test_settings.py
@@ -2,8 +2,17 @@ from .settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'postgres',  # Use the same database
+            'OPTIONS': {
+                'options': '-c search_path=pg_temp'  # Use temporary schema
+            }
+        }
     }
 }
-
