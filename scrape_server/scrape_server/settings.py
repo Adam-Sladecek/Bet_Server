1a1e7ff2c587f6caca98e1d3cda76fa7b41d90ea
Adam Sládeček <adamsladecek0@gmail.com>
2025-03-08 10:08:06 +0100

    changed conn age

--- scrape_server/scrape_server/settings.py
+++ scrape_server/scrape_server/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'PASSWORD': os.getenv("POSTGRES_PASSWORD", ''),
         'HOST': os.getenv("POSTGRES_HOST", ''),
         'PORT': os.getenv("POSTGRES_PORT", ''),
-        "CONN_MAX_AGE": int(os.getenv("CONN_AGE", '')),
+        "CONN_MAX_AGE": int(os.getenv("CONN_AGE", 0)),
     }
 }
 
